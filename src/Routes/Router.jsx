17ebf4747bf295d37e89267976c202c7f5298b11
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-10-19 22:55:31 +0600

    Login, Signup and Error Component Created Done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,9 @@ import {
 } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Signup/Signup";
+import Error from "../Pages/Error/Error";
 
 export const router = createBrowserRouter([
     {
@@ -12,7 +15,19 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
-            }
+            },
+            {
+                path: "login",
+                element: <Login></Login>
+            },
+            {
+                path: "signup",
+                element: <Signup></Signup>
+            },
         ]
+    },
+    {
+        path: "*",
+        element: <Error></Error>
     },
 ]);
