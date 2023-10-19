2273cfb23ed00f895c71af743a9b703335a622e3
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-10-19 23:27:33 +0600

    Login Page Setup Done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,104 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
+    const { signIn, signInWithGoogle, resetPassword } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const emailRef = useRef();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        // SignIn Call
+        signIn(email, password)
+            .then((result) => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true });
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Successful',
+                    text: 'You have successfully logged in.',
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Failed',
+                    text: 'There was an error logging in. Please check your credentials.',
+                });
+            });
+    };
+
+    const handleGoogleLogin = () => {
+        signInWithGoogle()
+            .then((result) => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true });
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Google Login Successful',
+                    text: 'You have successfully logged in with Google.',
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Google Login Failed',
+                    text: 'Error logging in with Google. Please try again later.',
+                });
+            });
+    };
     return (
-        <div>
-            <p>This is Login Page</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="mr-10 w-1/2">
+                    <img src="" alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl font-bold">Login now!</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" name='email' ref={emailRef} placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Login" className="btn btn-primary" />
+                            </div>
+                        </form>
+                        <p className='my-4 text-center'>New to this Site ? Please <Link className='text-amber-300' to='/signup'>Sign Up</Link></p>
+                        <button onClick={handleGoogleLogin} className="btn btn-outline"><FaGoogle></FaGoogle> SignIn With Google</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,68 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../Firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    // Create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Sign In User
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // Sign In with Google
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    // Logout
+    const logOut = () => {
+        setLoading(true);
+        signOut(auth);
+    }
+
+    // State Observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log('Loggedin user inside auth state', loggedUser);
+
+            setLoading(false);
+            setUser(loggedUser)
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
